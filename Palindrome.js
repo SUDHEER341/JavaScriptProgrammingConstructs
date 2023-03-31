d88ea6073c3fbc76cb0d53cefabd9691cc3c987f
SUDHEER341 <sudheer601220@gmail.com>
2023-03-31 08:45:06 +0530

    Added : program to check palindrome

---
+++ Palindrome.js
@@ -0,0 +1,15 @@
+function isPalindrome(str) {
+    // Convert  to lowercase
+    str = str.toLowerCase();
+  
+    for (var i = 0; i < str.length / 2; i++) {
+      if (str[i] !== str[str.length - 1 - i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+  
+   console.log(isPalindrome('racecar')); // true
+ 
+  
